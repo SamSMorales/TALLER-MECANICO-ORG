428121567eba8bf069e5ad5e3484c0f87c04602e
SamSMorales <109921652+SamSMorales@users.noreply.github.com>
2024-10-01 00:02:48 -0500

    Update PROYTALLERMECANICO.java

--- src/main/java/pe/com/taller/PROYTALLERMECANICO.java
+++ src/main/java/pe/com/taller/PROYTALLERMECANICO.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
  */
 
-package pe.com.avanzado.proy.taller.mecanico;
+package pe.com.taller;
 
 /**
  *
